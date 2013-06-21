5c46dc5802f909f03716d6b69752db771d86ab81
sylafrs <sylaf@hotmail.fr>
2013-06-21 14:32:20 +0200

    Add selection scene

---
+++ Unity/Assets/Scripts/Singletons/ChooseLeaderScene.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class ChooseLeaderScene
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class ChooseLeaderScene : MonoBehaviour {
+
+    private Hero selected;
+
+    private void Awake()
+    {
+        selected = null;
+    }
+
+    private void OnGUI()
+    {
+        if (Game.started)
+        {
+            Color old = GUI.color;        
+
+            GUILayout.Label("Choose hero : ");
+            GUILayout.BeginHorizontal();
+            foreach(var u in Game.settings.herosPrefabs) {
+                if (u == selected) GUI.color = Color.red;
+                if (GUILayout.Button(u.name))
+                {
+                    selected = u;
+                }
+                GUI.color = old;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        if (selected != null)
+        {
+            if (GUILayout.Button("Validate"))
+            {
+                Game.LaunchGame(selected);
+            }
+        }
+    }
+
+
+}
