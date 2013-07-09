410ae9d09978cde3709e83c8daaf0b89c1504278
sylafrs <sylaf@hotmail.fr>
2013-07-09 19:25:49 +0200

    basic attack system

---
+++ Unity/Assets/Scripts/Units/Projectile.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class Projectile
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class Projectile : MonoBehaviour {
+    public float speed;
+
+    public void OnDistanceLessThan(float distance, System.Action callback)
+    {
+
+    }
+
+    public void Release(Transform target)
+    {
+
+    }
+}
